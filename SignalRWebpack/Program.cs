df07d275efa54ae298e469619ca5113d6d480b91
thienandangthanh <thienandangthanh@gmail.com>
2022-11-07 11:55:41 +0700

    Add SignalR ChatHub

--- SignalRWebpack/Program.cs
+++ SignalRWebpack/Program.cs
@@ -1,3 +1,5 @@
+using SignalRWebpack.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -16,10 +20,13 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
+app.UseDefaultFiles();
+app.UseStaticFiles();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHub<ChatHub>("/hub");
 
 app.Run();
